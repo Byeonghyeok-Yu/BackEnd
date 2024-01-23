b4429cb1f145b6572d601eab0c90efa5ae792fea
Byeonghyeok-Yu <qudgur2215@naver.com>
2024-01-23 09:49:15 +0900

    first commit

---
+++ src/test/java/com/example/codepipeline/CodePipeLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePipeLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
